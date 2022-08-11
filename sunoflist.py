31da2b75eb8d1e5bc5f7eb47190cb75eee9ebe9c
Vishnu Narayanan V <100830902+VishnuNV007@users.noreply.github.com>
2022-08-11 20:57:46 +0530

    Add files via upload

---
+++ sunoflist.py
@@ -0,0 +1,7 @@
+#Python program to calculate the sum of a list of numbers
+numlist = [2,4,2,5,7,9,23,4,5]
+numsum = sum(numlist)
+print('Sum of List: ',numsum)
+
+numsum = sum(numlist, 5)
+print('Sum of List: ',numsum)
